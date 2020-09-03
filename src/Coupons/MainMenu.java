e9d4eadde4b8bae1ea8066b82012959be7432e1c
thayes8 <57459883+thayes8@users.noreply.github.com>
2020-09-03 14:29:17 -0500

    Initial Progress

--- src/Coupons/MainMenu.java
+++ src/Coupons/MainMenu.java
@@ -1,6 +1,7 @@
 package Coupons;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,18 +12,52 @@ public class MainMenu extends JFrame implements ActionListener {
      *
      */
     final int WIDTH = 400;
-    final int HEIGHT = 700;
+    final int HEIGHT = 600;
     public MainMenu(){
         this.setBounds(0, 0, WIDTH, HEIGHT);
         JPanel mainPanel = new JPanel();
+        JPanel playPanel = new JPanel();
+        JPanel tradePanel = new JPanel();
+        JPanel redeemPanel = new JPanel();
+        mainPanel.setBounds(0, 0, WIDTH, HEIGHT);
         this.add(mainPanel);
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
-        JButton tradeButton = new JButton("Trade");
+        BoxLayout layout = new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS);
+        mainPanel.setLayout(layout);
+
+        JButton tradeButton = new JButton("TRADE");
+        JButton playButton = new JButton("PLAY");
+        JButton redeemButton = new JButton("REDEEM");
+        playButton.setFont(new Font(Font.MONOSPACED, Font.BOLD, 75));
+        tradeButton.setFont(new Font(Font.MONOSPACED, Font.BOLD, 75));
+        redeemButton.setFont(new Font(Font.MONOSPACED, Font.BOLD, 75));
+        playButton.setBounds(WIDTH/3, HEIGHT/4, WIDTH, HEIGHT/3);
+        redeemButton.setBounds(WIDTH/3, 2*HEIGHT/4, WIDTH, HEIGHT/3);
+        tradeButton.setBounds(WIDTH/3, 3*HEIGHT/4, WIDTH, HEIGHT/3);
+        //        playButton.setMinimumSize(new Dimension(WIDTH, HEIGHT/3));
+//        tradeButton.setMinimumSize(new Dimension(WIDTH, HEIGHT/3));
+//        redeemButton.setMinimumSize(new Dimension(WIDTH, HEIGHT/3));
+
+        playButton.setOpaque(true);
+        tradeButton.setOpaque(true);
+        redeemButton.setOpaque(true);
+        playButton.setForeground(new Color(145, 0, 7));
+        playButton.setBackground(new Color(0xDC0A00));
+        tradeButton.setForeground(new Color(145, 0, 7));
+        tradeButton.setBackground(new Color(0xDC0A00));
+        redeemButton.setForeground(new Color(145, 0, 7));
+        redeemButton.setBackground(new Color(0xDC0A00));
+
+        playPanel.add(playButton);
+        tradePanel.add(tradeButton);
+        redeemPanel.add(redeemButton);
+
+        mainPanel.add(playPanel);
+        mainPanel.add(tradePanel);
+        mainPanel.add(redeemPanel);
 
         tradeButton.addActionListener(this);
-        mainPanel.add(tradeButton);
-
-
+        playButton.addActionListener(this);
+        redeemButton.addActionListener(this);
         this.setTitle("Main Menu");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
--- src/Coupons/TradeMenu.java
+++ src/Coupons/TradeMenu.java
@@ -1,7 +1,22 @@
 package Coupons;
 
-public class TradeMenu{
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TradeMenu extends JFrame implements ActionListener {
     /**
      * This is the first screen after clicking trade in the main menu
      */
+    public TradeMenu(){
+        this.setBounds(0, 0, WIDTH, HEIGHT);
+        JPanel mainPanel = new JPanel();
+        JPanel friendTradePanel = new JPanel();
+        JPanel randomTradePanel = new JPanel();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
 }
